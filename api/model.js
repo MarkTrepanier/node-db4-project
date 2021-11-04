7538b38a3ab55690c2c63f993b7662e0ae76778b
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-11-04 19:56:18 -0400

    get by id returning array

--- api/model.js
+++ api/model.js
@@ -4,6 +4,51 @@ async function getRecipes() {
   return await db("recipes");
 }
 
+// select rc.*, st.step_id, st.step_number, st.instructions, ig.ingredient_id, ig.ingredient_name
+// from recipes as rc
+// join steps as st
+// on rc.recipe_id = st.recipe_id
+// join steps_ingredients as si
+// on st.step_id = si.step_id
+// left join ingredients as ig
+// on ig.ingredient_id = si.ingredient_id
+// where rc.recipe_id = 2
+async function getRecipeById(recipe_id) {
+  const recipe = await db("recipes as rc")
+    .join("steps as st", "rc.recipe_id", "st.recipe_id")
+    .join("steps_ingredients as si", "st.step_id", "si.step_id")
+    .leftJoin("ingredients as ig", "ig.ingredient_id", "si.ingredient_id")
+    .select(
+      "rc.*",
+      "st.step_id",
+      "st.step_number",
+      "st.instructions",
+      "ig.ingredient_id",
+      "ig.ingredient_name"
+    )
+    .where("rc.recipe_id", recipe_id);
+
+  return recipe;
+  //   const result = {
+  //     steps: [],
+  //   };
+
+  //   for (let step of scheme) {
+  //     if (!result.scheme_id) {
+  //       result.scheme_id = Number(scheme_id);
+  //       result.scheme_name = step.scheme_name;
+  //     }
+  //     if (step.step_id) {
+  //       result.steps.push({
+  //         step_id: step.step_id,
+  //         step_number: step.step_number,
+  //         instructions: step.instructions,
+  //       });
+  //     }
+  //   }
+}
+
 module.exports = {
   getRecipes,
+  getRecipeById,
 };
--- api/router.js
+++ api/router.js
@@ -4,12 +4,19 @@ const Recipe = require("./model");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  console.log("test1");
   Recipe.getRecipes()
     .then((recipes) => {
-      console.log("test2");
       res.status(200).json(recipes);
     })
     .catch();
 });
+
+router.get("/:recipe_id", (req, res) => {
+  Recipe.getRecipeById(req.params.recipe_id)
+    .then((resp) => {
+      res.status(200).json(resp);
+    })
+    .catch();
+});
+
 module.exports = router;
