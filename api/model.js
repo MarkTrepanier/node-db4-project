c14d67fad1df27024354d36786356cd5cc2f23ef
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-11-04 21:31:34 -0400

    alomst there!

--- api/model.js
+++ api/model.js
@@ -4,15 +4,6 @@ async function getRecipes() {
   return await db("recipes");
 }
 
-// select rc.*, st.step_id, st.step_number, st.instructions, ig.ingredient_id, ig.ingredient_name
-// from recipes as rc
-// join steps as st
-// on rc.recipe_id = st.recipe_id
-// join steps_ingredients as si
-// on st.step_id = si.step_id
-// left join ingredients as ig
-// on ig.ingredient_id = si.ingredient_id
-// where rc.recipe_id = 2
 async function getRecipeById(recipe_id) {
   const recipe = await db("recipes as rc")
     .join("steps as st", "rc.recipe_id", "st.recipe_id")
@@ -28,24 +19,32 @@ async function getRecipeById(recipe_id) {
     )
     .where("rc.recipe_id", recipe_id);
 
-  return recipe;
-  //   const result = {
-  //     steps: [],
-  //   };
+  const result = {
+    steps: [],
+  };
 
-  //   for (let step of scheme) {
-  //     if (!result.scheme_id) {
-  //       result.scheme_id = Number(scheme_id);
-  //       result.scheme_name = step.scheme_name;
-  //     }
-  //     if (step.step_id) {
-  //       result.steps.push({
-  //         step_id: step.step_id,
-  //         step_number: step.step_number,
-  //         instructions: step.instructions,
-  //       });
-  //     }
-  //   }
+  for (let step of recipe) {
+    if (!result.recipe_id) {
+      result.recipe_id = Number(recipe_id);
+      result.recipe_name = step.recipe_name;
+    }
+    if (step.step_id) {
+      const aStep = {
+        step_id: step.step_id,
+        step_number: step.step_number,
+        instructions: step.instructions,
+        ingredients: [],
+      };
+      for (let step of recipe) {
+        if (step.step_id === aStep.step_id)
+          aStep.ingredients.push({
+            ingredient_name: step.ingredient_name,
+          });
+      }
+      result.steps.push(aStep);
+    }
+  }
+  return result;
 }
 
 module.exports = {
